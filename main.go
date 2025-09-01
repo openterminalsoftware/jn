078927711b27b1598710b181f79ec2bd5f63602d
myferr <contactme.myfer@protonmail.com>
2025-09-01 03:07:50 -0500

    refactor(colors): Extract color constants to separate package

--- main.go
+++ main.go
@@ -2,40 +2,28 @@ package main
 
 import (
 	"fmt"
+	"jn/colors"
 	"os"
 )
 
-const (
-	Red      = "\033[31m"
-	Green    = "\033[32m"
-	Yellow   = "\033[33m"
-	Blue     = "\033[34m"
-	Magenta  = "\033[35m"
-	Cyan     = "\033[36m"
-	Reset    = "\033[0m"
-	Bold     = "\033[1m"
-	Italic   = "\033[3m"
-	DarkGray = "\033[90m"
-)
-
 func help() {
-	fmt.Println(Bold + Magenta + "jn" + Reset)
-	fmt.Println(Bold + Blue + "— Markdown previewer and journal/note-taking command line tool written in Go. " + Reset)
+	fmt.Println(colors.Bold + colors.Magenta + "jn" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Blue + "— Markdown previewer and journal/note-taking command line tool written in Go. " + colors.Reset)
 	fmt.Println("\nUsage:")
-	fmt.Println(Bold + "  jn " + Reset + Italic + "<command>" + " [arguments]" + Reset)
-	fmt.Println(Bold + "  jn " + Reset + Italic + "<file>" + Reset)
+	fmt.Println(colors.Bold + "  jn " + colors.Reset + colors.Italic + "<command>" + " [arguments]" + colors.Reset)
+	fmt.Println(colors.Bold + "  jn " + colors.Reset + colors.Italic + "<file>" + colors.Reset)
 	fmt.Println("\nCommands:")
-	fmt.Println(Bold + Cyan + "  help" + Reset + Green + "            Alias: h" + Reset)
-	fmt.Println(Bold + Cyan + "  new" + Reset + Green + "             Alias: n" + Reset)
-	fmt.Println(Bold + Cyan + "  preview" + Reset + Green + "         Alias: p" + Reset)
-	fmt.Println(Bold + Cyan + "  publish" + Reset + Green + "         Alias: pub" + Reset)
-	fmt.Println(Bold + Cyan + "  delete" + Reset + Green + "          Alias: d" + Reset)
-	fmt.Println(Bold + Cyan + "  list" + Reset + Green + "            Alias: l" + Reset)
-	fmt.Println(Bold + Cyan + "  search" + Reset + Green + "          Alias: s" + Reset)
-	fmt.Println(Bold + Cyan + "  version" + Reset + Green + "         Alias: v" + Reset)
-	fmt.Println(Bold + Cyan + "  config" + Reset + Green + "          Alias: conf" + Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  help" + colors.Reset + colors.Green + "            Alias: h" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  new" + colors.Reset + colors.Green + "             Alias: n" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  preview" + colors.Reset + colors.Green + "         Alias: p" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  publish" + colors.Reset + colors.Green + "         Alias: pub" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  delete" + colors.Reset + colors.Green + "          Alias: d" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  list" + colors.Reset + colors.Green + "            Alias: l" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  search" + colors.Reset + colors.Green + "          Alias: s" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  version" + colors.Reset + colors.Green + "         Alias: v" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  config" + colors.Reset + colors.Green + "          Alias: conf" + colors.Reset)
 
-	fmt.Println("\n\n" + DarkGray + "jn help <command> for more information on a command." + Reset)
+	fmt.Println("\n\n" + colors.DarkGray + "jn help <command> for more information on a command." + colors.Reset)
 
 }
 
