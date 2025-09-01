d9d2178b3fdef23d72880726af1618e4ec9a39ca
myferr <contactme.myfer@protonmail.com>
2025-09-01 03:10:36 -0500

    Refactor: Extract help function to prompts package

---
+++ prompts/prompts.go
@@ -0,0 +1,26 @@
+package prompts
+
+import (
+	"fmt"
+	"jn/colors"
+)
+
+func Help() {
+	fmt.Println(colors.Bold + colors.Magenta + "jn" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Blue + "— Markdown previewer and journal/note-taking command line tool written in Go. " + colors.Reset)
+	fmt.Println("\nUsage:")
+	fmt.Println(colors.Bold + "  jn " + colors.Reset + colors.Italic + "<command>" + " [arguments]" + colors.Reset)
+	fmt.Println(colors.Bold + "  jn " + colors.Reset + colors.Italic + "<file>" + colors.Reset)
+	fmt.Println("\nCommands:")
+	fmt.Println(colors.Bold + colors.Cyan + "  help" + colors.Reset + colors.Green + "            Alias: h" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  new" + colors.Reset + colors.Green + "             Alias: n" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  preview" + colors.Reset + colors.Green + "         Alias: p" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  publish" + colors.Reset + colors.Green + "         Alias: pub" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  delete" + colors.Reset + colors.Green + "          Alias: d" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  list" + colors.Reset + colors.Green + "            Alias: l" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  search" + colors.Reset + colors.Green + "          Alias: s" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  version" + colors.Reset + colors.Green + "         Alias: v" + colors.Reset)
+	fmt.Println(colors.Bold + colors.Cyan + "  config" + colors.Reset + colors.Green + "          Alias: conf" + colors.Reset)
+
+	fmt.Println("\n\n" + colors.DarkGray + "jn help <command> for more information on a command." + colors.Reset)
+}
